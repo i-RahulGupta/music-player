4ecae5d6b6f5dcdf96b0c25a67379603dfe547c1
Rahul Gupta <130181558+i-RahulGupta@users.noreply.github.com>
2023-04-10 18:13:22 +0530

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,45 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+    {
+      name: "I Like Me Better",
+      artist: "Lauv",
+      img: "music-1",
+      src: "music-1"
+    },
+    {
+      name: "Stay",
+      artist: "Justin Bieber Kid Laroi",
+      img: "music-2",
+      src: "music-2"
+    },
+    {
+      name: "Teenage Dream",
+      artist: "Stephen Dawes",
+      img: "music-3",
+      src: "music-3"
+    },
+    {
+      name: "Look at me now",
+      artist: "Levent Geiger",
+      img: "music-4",
+      src: "music-4"
+    },
+
+    // like this paste it and remember to give comma after ending of this bracket }
+    // {
+    //   name: "Here is the music name",
+    //   artist: "Here is the artist name",
+    //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+    //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+    // }
+  ];
